ebbfe8377cb1b174a4922de64dae4e652b526371
bayuik <bayuindrakusuma05@gmail.com>
2020-12-25 19:33:46 +0700

    Build a simple game, TargetSum

---
+++ src/components/Game.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    View,
+    StyleSheet, 
+    Text,
+} from 'react-native';
+
+class Game extends React.Component {
+    static propTypes = {
+        randomNumberCount: PropTypes.number.isRequired,
+    }
+    randomNumbers = Array
+        .from({length: this.props.randomNumberCount})
+        .map(() => 1 + Math.floor(10 * Math.random()));
+    target = this.randomNumbers
+        .slice(0, this.props.randomNumberCount - 2)
+        .reduce((acc, curr) => acc + curr, 0);
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.target}>{this.target}</Text>
+                {this.randomNumbers.map((randomNumber, index) => 
+                    <Text key={index}>{randomNumber}</Text>
+                )}
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ddd',
+        flex: 1,
+    },
+    target: {
+        fontSize: 40,
+        backgroundColor: '#aaa',
+        marginHorizontal: 50,
+        textAlign: 'center',
+    }
+})
+
+export default Game;
